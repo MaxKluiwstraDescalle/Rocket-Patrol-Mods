dc4da03eac1bf77094408cc10587f6c3c0cd8df2
Max-Emilien Kluiwstra-Descalle <mkluiwst@ucsc.edu>
2024-01-31 21:41:42 -0800

    updated for no warnings

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,12 +8,13 @@ class Menu extends Phaser.Scene {
         this.load.image('spaceship', './assets/spaceship.png')
         this.load.image('starfield','./assets/starfield.png')
         this.load.image('ShipF','./assets/Ship2.png')
-        this.load.spritesheet('explosion', './assets/explosion.png',{
+        /*this.load.spritesheet('explosion', './assets/explosion.png',{
             frameWidth: 64,
             frameHeight: 32,
             startFrame: 0,
             endFrame: 9
-        })
+        })*/
+        this.load.image('explosion','./assets/explosion.png')
         this.load.audio('sfx-select', './assets/sfx-select.wav')
         this.load.audio('sfx-explosion', './assets/sfx-explosion.wav')
         this.load.audio('sfx-shot', './assets/sfx-shot.wav')
@@ -24,7 +25,7 @@ class Menu extends Phaser.Scene {
     }
 
     create(){
-        this.anims.create({
+        /*this.anims.create({
             key: 'explode',
             frames: this.anims.generateFrameNumbers('explosion', {
                 start:0, 
@@ -33,7 +34,7 @@ class Menu extends Phaser.Scene {
             }),
             frameRate: 30
         })
-
+        */
         let menuConfig= {
             fontFamily: 'Georgia',
             fontSize: '72px',
